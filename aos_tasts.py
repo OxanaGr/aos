faf33d5b7df844f94ea7a4e5d88791a355abfa21
OxanaGr <100324401+OxanaGr@users.noreply.github.com>
2022-03-14 02:03:56 -0700

    Add files via upload

---
+++ aos_tasts.py
@@ -0,0 +1,17 @@
+import unittest
+import aos_methods as methods
+
+
+class aosPositiveTestCases(unittest.TestCase):
+    @staticmethod
+    def test_CrtNwAcnt():
+        methods.SetUp()
+        methods.CrtNwAcnt()
+        methods.log_out()
+        methods.Login()
+        methods.log_out()
+        methods.Checking_homepage_texts()
+        methods.links_top_nav_menu_and_logo()
+        methods.Check_CONTACT_US()
+        methods.Check_Social_Media_links()
+        methods.TearDown()
