1d88830db27ba210075ef5a736e275821c30756c
OxanaGr <100324401+OxanaGr@users.noreply.github.com>
2022-03-14 02:03:20 -0700

    Add files via upload

---
+++ aos_locators.py
@@ -0,0 +1,18 @@
+import it as it
+from faker import Faker
+fake = Faker(locale='en_CA')
+
+email = fake.email()
+password = fake.password()
+first_name = fake.first_name()
+last_name = fake.last_name()
+phone_number = fake.phone_number()
+city = fake.city()
+address = fake.address().replace("\n"," ")[:49]
+province = fake.province()[:3]
+username = f'{first_name}{last_name}'[:15]
+postal_code = fake.postalcode()
+subject = 'I Like it!'
+
+Main_Page = 'https://advantageonlineshopping.com/'
+Title = 'Advantage Shopping'
